114231e4297b1747768be2878c7c199682423d5a
Enc1102 <eli.nebotcolomer@gmail.com>
2025-03-31 14:28:06 +0200

    modif

--- Nebot-Colomer-Elisabet-PEC1_Script.R
+++ Nebot-Colomer-Elisabet-PEC1_Script.R
@@ -12,7 +12,7 @@ library(readr);library(SummarizedExperiment);library(tidyverse); library(xfun); 
 #1. Seleccionad y descargad un dataset de metabolómica, que podéis obtener de metabolomicsWorkbench o de este repositorio de GitHub.
 
 human_cachexia <- read_csv("Data/human_cachexia.csv")
-str(human_cachexia)
+str(human_cachexia) # se visualiza la estructura del dataset
 
 #2. Cread un objeto de clase SummarizedExperiment que contenga los datos y los metadatos (información acerca del dataset, sus filas y columnas). La clase SummarizedExperiment es una extensión de ExpressionSet, utilizada por muchas aplicaciones y bases de datos (como es el caso de metabolomicsWorkbench).
 #¿Cuáles son sus principales diferencias con la clase ExpressionSet?
@@ -42,10 +42,10 @@ se_object <- SummarizedExperiment(
 se_object
 
 assay(se_object) # se visualiza la matriz de datos
-colData(se_object) 
-metadata(se_object)
+colData(se_object) #se visualiza la ID de los pacientes y el factor 'Muscle.loss'
+metadata(se_object) # se visualiza la descripción del estudio
 
-save(se_object, file = "SummarizedExperiment_data.Rda")
+save(se_object, file = "SummarizedExperiment_data.Rda") # se guarda el 'se_object' como archivo .Rda
 
 
 #3. Llevad a cabo un análisis exploratorio que os proporcione una visión general del dataset en la línea de lo que hemos visto en las actividades de este reto.
@@ -65,8 +65,8 @@ dataframe <- dataframe  %>%
   left_join(as.data.frame(colData(se_object)) %>% 
               rownames_to_column(var = "Patient_ID"), by = "Patient_ID") # se une la columna de 'Muscle.loss'
 
-dim(dataframe)
-names(dataframe)
+dim(dataframe) # se visualiza las dimensiones de este dataframe
+names(dataframe) # se visualiza los nombres de las columnas del dataframe
 
 
 # Se crea histogramas por paciente
@@ -158,22 +158,23 @@ ggsave("Outputs/Boxplot_plot3.png", plot = boxplot3, width = 20, height = 15, dp
 
 # Realizar PCA sobre los datos de expresión de metabolitos
 log_counts <- log10(t(assay(se_object, "counts")) + 1) #transformo los datos con el logaritmo en base 10.
-pca_res_log <- prcomp(log_counts, scale. = TRUE)
+
+pca_res_log <- prcomp(log_counts, scale. = TRUE) # se obtiene las coordenadas de cada PC por metabolito
 
 # Extraer las coordenadas de los primeros dos componentes principales (PC1 y PC2)
-pca_df <- as.data.frame(pca_res_log$x)  # Esto contiene las puntuaciones de los componentes principales
-pca_df$Muscle.loss <- colData(se_object)$`Muscle loss`  # Añadir la columna de metadatos (Muscle.loss)
+pca_df <- as.data.frame(pca_res_log$x)  # Esto extrae la x(coordenadas de PC) y convierte las coordenadas en dataframe
+pca_df$Muscle.loss <- colData(se_object)$`Muscle loss`  # Se añade la columna de metadatos (Muscle.loss)
 
 # Extraer las coordenadas de los metabolitos
-pca_var <- as.data.frame(pca_res_log$rotation[, 1:2])  
-pca_var$Metabolito <- rownames(pca_var)  # Nombres de metabolitos
+pca_var <- as.data.frame(pca_res_log$rotation[, 1:2]) #  extrae las columnas del PC1 y PC2 y lo convierte en dataframe
+pca_var$Metabolito <- rownames(pca_var)  # Añade una columna con los nombres de metabolitos
 
 
 ## Determinar el porcentaje de variación explicado por cada PC.
-std_devs <- pca_res_log$sdev
-eigenvalues <- std_devs^2
+std_devs <- pca_res_log$sdev # extrae los desviación estándar del objeto pca_res_log
+eigenvalues <- std_devs^2 # cálculo para obtener la varianza.
 variance_explained <- eigenvalues / sum(eigenvalues)
-cumulative_variance <- cumsum(variance_explained)
+cumulative_variance <- cumsum(variance_explained) # porcentaje de variación explicada por los PC.
 
 # PCA del PC1 y Pc2 
 
